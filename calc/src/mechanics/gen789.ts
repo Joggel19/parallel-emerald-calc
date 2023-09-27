d2377ab15bab45c30e3277dcd939ff7994b01ef5
Joggel19 <jonathan.vorndran@web.de>
2023-09-27 16:01:58 +0200

    more layout changes

--- calc/src/mechanics/gen789.ts
+++ calc/src/mechanics/gen789.ts
@@ -500,7 +500,9 @@ export function calculateSMSSSV(
       baseDamage = pokeRound(OF32(baseDamage * 2048) / 4096);
       desc.weather = field.weather;
     }
-  } else if (field.hasWeather('Hail') && attacker.hasType('Ice')) {
+  } 
+  
+  if (field.hasWeather('Hail') && attacker.hasType('Ice')) {
     baseDamage = pokeRound(OF32(baseDamage * 5324) / 4096);
     desc.weather = field.weather;
   }
